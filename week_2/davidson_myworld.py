49397c8d338aa6a3bc0eaecdb678a3d35a0632c2
Jojo <davidson.jojo@yahoo.com>
2023-08-18 20:46:38 -0500

    week 2 completed

---
+++ week_2/davidson_myworld.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------
+#  Title: davidson_myworld.py
+#  Author: John Davidson
+#  Date: 08 August 2023
+#  Description: Exercise 2.3 for WEB 335 - Intro to NoSQL.
+#-------------------------------------------------------
+
+last_name = "Davidson"
+greeting = f"You are now in {last_name}'s world!"
+
+print(greeting)
